aad2b3b3db48b3a7cd3f6c64bc8820b590cbde4f
evergreen-alex-emery <alex.emery@evergreen-life.co.uk>
2020-07-21 18:59:20 +0100

    Initial Commit

---
+++ Control Deck/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Control Deck
+//
+//  Created by Alexander Emery on 21/07/2020.
+//  Copyright © 2020 Alexander Emery. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
